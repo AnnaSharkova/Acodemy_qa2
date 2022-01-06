dcac083447d271d1b14a52b6686296250de8472e
annasharkova <annaa.sharkova@gmail.com>
2022-01-06 11:28:22 +0200

    [feature/java1]: Add new information

---
+++ src/main/java/Homework/PrimitiveTypes.java
@@ -0,0 +1,183 @@
+package Homework;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+//        Floating point example
+
+        // Float (Classroom)
+        float floatExample = 999999.8F;
+        System.out.println(floatExample);
+        System.out.println(999999.8F);
+
+//       Float (My examples)
+
+        float floatExampleOne = 225.55F;
+        System.out.println(floatExampleOne);
+        System.out.println(225.55F);
+
+        float floatExampleTwo = 333.3F;
+        System.out.println(floatExampleTwo);
+        System.out.println(333.3F);
+
+        float floatExampleThree = 415.45F;
+        System.out.println(floatExampleThree);
+        System.out.println(415.45F);
+
+        float floatExampleFour = 520.654F;
+        System.out.println(floatExampleFour);
+        System.out.println(520.654F);
+
+        float floatExampleFive = 65432.1F;
+        System.out.println(floatExampleFive);
+        System.out.println(65432.1F);
+
+
+        float floatExampleSix = 789.789F;
+        System.out.println(floatExampleSix);
+        System.out.println(789.789F);
+
+        float floatExampleSeven = 121212.19F;
+        System.out.println(floatExampleSeven);
+        System.out.println(121212.19F);
+
+        float floatExampleEight = 456789.123F;
+        System.out.println(floatExampleEight);
+        System.out.println(456789.123F);
+
+        float floatExampleNine = 2.25F;
+        System.out.println(floatExampleNine);
+        System.out.println(2.25F);
+
+        float floatExampleTen = 77777.987F;
+        System.out.println(floatExampleTen);
+        System.out.println(77777.987F);
+
+
+        // Double (Classroom)
+        double doubleExample = 2000000.9876D;
+        System.out.println(doubleExample);
+        System.out.println(2000000.9876D);
+
+//        Double (My examples)
+
+        double doubleExampleOne = 1000.22D;
+        System.out.println(doubleExampleOne);
+        System.out.println(100.22D);
+
+        double doubleExampleTwo = 1000.22D;
+        System.out.println(doubleExampleTwo);
+        System.out.println(100.22D);
+
+        double doubleExampleThree = 30005000.5D;
+        System.out.println(doubleExampleThree);
+        System.out.println(30005000.5D);
+
+        double doubleExampleFour = 440056789.567D;
+        System.out.println(doubleExampleFour);
+        System.out.println(440056789.567D);
+
+        double doubleExampleFive = 55005500.55D;
+        System.out.println(doubleExampleFive);
+        System.out.println(55005500.55D);
+
+        double doubleExampleSix = 67890.6789D;
+        System.out.println(doubleExampleSix);
+        System.out.println(67890.6789D);
+
+        double doubleExampleSeven = 770.77D;
+        System.out.println(doubleExampleSeven);
+        System.out.println(770.77D);
+
+        double doubleExampleEight = 8888888.98D;
+        System.out.println(doubleExampleEight);
+        System.out.println(8888888.98D);
+
+        double doubleExampleNine = 999.9876D;
+        System.out.println(doubleExampleNine);
+        System.out.println(999.9876D);
+
+        double doubleExampleTen = 10.1D;
+        System.out.println(doubleExampleTen);
+        System.out.println(10.1D);
+
+
+
+//        Integral example
+
+        // Byte (classroom)
+        byte handCount = 2;
+        System.out.println(handCount);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        // Short (classroom)
+        short distanceBetweenKievAndRiga = 1011;
+        System.out.println(distanceBetweenKievAndRiga);
+
+        // Integer (classroom)
+        int populationOfLatvia = 1900000;
+        System.out.println(populationOfLatvia);
+
+        // Long (classroom)
+        long currentWorldPopulation = 7900000000L;
+        System.out.println(currentWorldPopulation);
+
+        // Char (classroom)
+        char ch = '\u03A9';
+        char pUnicode = '\u0070';
+        char[] arrayOfChars = {'a', 'b', 'c'};
+        System.out.println(Arrays.toString(arrayOfChars));
+        char p = 'p';
+        System.out.println(ch);
+        System.out.println(p);
+
+
+        int[] arrayOfPeopleAges = {21, 22, 23, 24};
+        System.out.println(Arrays.toString(arrayOfPeopleAges));
+
+        // String variables
+
+        String myName = "Nikita";
+        System.out.println(myName);
+
+        String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempus pharetra iaculis. Quisque at feugiat sem.";
+        System.out.println(loremIpsum);
+
+        String mySurname = "Milka";
+
+//        String myNameAndUsername = myName + mySurname;
+//        String myNameAndUsername = myName + " " + mySurname;
+//        System.out.println(myNameAndUsername);
+
+        // %s = string
+        // %d = int
+
+        System.out.printf("My name is %s. My surname is %s.\n", myName, mySurname);
+        System.out.println("My name is " + myName + "." + " " + "My surname is " + mySurname + ".");
+
+
+        // Operators
+
+        int sumOfTwoNumbers = 10 + 30;
+        System.out.println(sumOfTwoNumbers);
+
+        System.out.println(5 + 10);
+        System.out.println("5 + 10");
+
+        int a = 30;
+        int b = 5;
+
+        System.out.println(a / b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a + b);
+
+        System.out.println(9 % 4);
+
+        // 4 4 = 8
+        // Result: 1
+
+        float doubleDivision = 100 / 13F;
+        System.out.println(doubleDivision);
+    }
+}
---
+++ src/main/java/classroom/PrimitiveTypes.java
@@ -0,0 +1,8 @@
+package classroom;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+//
+    }
+}
