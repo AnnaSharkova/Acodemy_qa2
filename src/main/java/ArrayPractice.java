9d1e31ac362333cf884c9bb5faca21b8f663622d
annasharkova <annaa.sharkova@gmail.com>
2022-01-20 10:26:40 +0300

    [feature/java1]: Add new information

---
+++ src/main/java/ArrayPractice.java
@@ -0,0 +1,17 @@
+public class ArrayPractice {
+    public static void main(String[] args) {
+/* If X = 0
+Print HelloWorld
+
+
+
+ */
+        int x = 1;
+        if (x == 0){
+            System.out.println("HelloWorld");
+        } else {
+            System.out.println("Bye Bye World");
+        }
+
+    }
+}
---
+++ src/main/java/classwork/LogicalOperators.java
@@ -0,0 +1,7 @@
+package classwork;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+    }
+}
