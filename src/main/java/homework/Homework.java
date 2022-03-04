2b8b12e15d2506aa62e34250206ab0b6afe72afc
annasharkova <annaa.sharkova@gmail.com>
2022-03-04 12:03:49 +0200

    [feature/java1]: Add new information

---
+++ src/main/java/homework/Homework.java
@@ -0,0 +1,45 @@
+package homework;
+
+public class Homework {
+    public static void main(String[] args) {
+
+//  If int = 10, print "Integer value is 10"
+
+        int x = 10;
+        if (x == 10) {
+            System.out.println("Integer value is 10");
+        }
+
+    }
+
+/* Int = 18-65
+If you are 18-21, your discount in dry cleaning is 10%.
+If you are 22-65, your discount in dry cleaning is 15%.
+If you are older than 65, your discount is 20%.
+*/
+
+
+    public static void main(String[] args) {
+      int humanAge = 35;
+        if(humanAge = 18; humanAge <= 21){
+            System.out.println("Your discount im dry cleaning is 10%");
+        }else if (humanAge >= 22; humanAge <= 65){
+            System.out.println("Your discount in dry cleaning is 15%");
+        }else if (humanAge > 65) {
+            System.out.println("You are old human, your discount is 20%");
+        }else (humanAge > 18) {
+            System.out.println("You are too young human, go away");
+        }
+    }
+
+
+//  3.Create array, put into array 10 animals
+
+public static void main(String[] args) {
+    String[]animalsArray = {"cat", "dog", "panda", "horse", "pig", "snake", "sheep", "bear", "fox", "bull"};
+    System.out.println(animalsArray[4]);
+}
+
+
+
+}
